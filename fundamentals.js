18234d80bcb773ca9c2b0153e9272d2863a6cfbb
williamvasilev <williamvasilev@gmail.comgit config --global user.name William>
2022-09-19 13:40:44 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -9,7 +9,17 @@ const aCar = {
       countycode : 'WD',
       number : '1058'
     },
+    mileage : '10000',
+    colour : {
+      exterior : 'red',
+      interiorfbric : {
+        texture : 'leather',
+        shade : 'cream'
+      }
+
+    }
   };
   
   console.log(aCar.owner + ' drives a ' + aCar.type.make)
   console.log(aCar.registration.year + '-' + aCar.registration.countycode + '-' + aCar.registration.number)
+  console.log('It is a ' + aCar.colour.exterior + ' car,' + aCar.mileage + ' mileage, with ' + aCar.colour.interiorfbric.texture + ' interior')
